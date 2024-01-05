ebcad30486ba1af72f3067f51ce611b7835bdb4d
Prathap. s <88225648+068prathap@users.noreply.github.com>
2024-01-05 20:26:12 +0530

    Update slice.js
    changed card and pin no

--- src/services/slice.js
+++ src/services/slice.js
@@ -2,9 +2,9 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
     usersDetails: [
-        { card_no: 42424242424242, cust_name: "Vishva", pin: 4545, isValid: false, availableBalance: 625.30 },
-        { card_no: 62000000000047, cust_name: "Satheesh", pin: 6060, isValid: true, availableBalance: 10852.00 },
-        { card_no: 36227206271667, cust_name: "Boopesh", pin: 7485, isValid: true, availableBalance: 6254.40 }
+        { card_no: 11111111111111, cust_name: "Vishva", pin: 1111, isValid: false, availableBalance: 625.30 },
+        { card_no: 22222222222222, cust_name: "Satheesh", pin: 2222, isValid: true, availableBalance: 10852.00 },
+        { card_no: 33333333333333, cust_name: "Boopesh", pin: 3333, isValid: true, availableBalance: 6254.40 }
     ],
     user: '',
     result: '',
